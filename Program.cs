0bb7271c69efe7361113a2c8fb3a8e01396f83b4
Mbakig <mbakiguven797@gmail.com>
2024-10-05 20:39:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Kullanıcıdan iki sayı al
+        Console.Write("Birinci sayıyı girin: ");
+        double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("İkinci sayıyı girin: ");
+        double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+        // Geometrik ortalamayı hesapla
+        double geometrikOrtalama = Math.Sqrt(sayi1 * sayi2);
+
+        // Sonucu ekrana yazdır
+        Console.WriteLine($"Geometrik Ortalama: {geometrikOrtalama}");
+    }
+}
