21e4ebcbcb6e173cd2c1dff0b2b382f60d48e998
Mbakig <mbakiguven797@gmail.com>
2024-11-30 22:08:50 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -534,147 +534,429 @@
 //    }
 //}
 
+//using System;
+//using System.Collections.Generic;
+
+//class Program
+//{
+
+//    public class Urun
+//    {
+//        public string Adi { get; set; }
+//        public decimal BirimFiyat { get; set; }
+//        public int Adet { get; set; }
+
+//        public Urun(string adi, decimal birimFiyat, int adet)
+//        {
+//            Adi = adi;
+//            BirimFiyat = birimFiyat;
+//            Adet = adet;
+//        }
+//    }
+
+
+//    public class Depo
+//    {
+//        public List<Urun> Urunler { get; set; }
+
+//        public Depo()
+//        {
+//            Urunler = new List<Urun>();
+//        }
+
+//        public void UrunEkle(string adi, decimal birimFiyat, int adet)
+//        {
+//            var mevcutUrun = Urunler.Find(u => u.Adi == adi);
+//            if (mevcutUrun != null)
+//            {
+//                mevcutUrun.Adet += adet;
+//            }
+//            else
+//            {
+//                Urunler.Add(new Urun(adi, birimFiyat, adet));
+//            }
+//            Console.WriteLine($"{adi} ürünü depoya eklendi.");
+//        }
+
+
+//        public bool UrunSat(string adi, int adet, decimal param)
+//        {
+//            var urun = Urunler.Find(u => u.Adi == adi);
+//            if (urun == null)
+//            {
+//                Console.WriteLine("Depoda bu ürün bulunmamaktadır.");
+//                return false;
+//            }
+//            if (urun.Adet < adet)
+//            {
+//                Console.WriteLine("Depoda yeterli adet yok.");
+//                return false;
+//            }
+
+//            decimal toplamTutar = urun.BirimFiyat * adet;
+//            if (param < toplamTutar)
+//            {
+//                Console.WriteLine("Yeterli paranız yok.");
+//                return false;
+//            }
+
+//            urun.Adet -= adet;
+//            Console.WriteLine($"{adet} adet {adi} satıldı. Toplam tutar: {toplamTutar} TL.");
+//            return true;
+//        }
+
+
+//        public void UrunleriListele()
+//        {
+//            if (Urunler.Count == 0)
+//            {
+//                Console.WriteLine("Depoda hiç ürün yok.");
+//                return;
+//            }
+//            foreach (var urun in Urunler)
+//            {
+//                Console.WriteLine($"Ürün: {urun.Adi}, Birim Fiyatı: {urun.BirimFiyat} TL, Stok: {urun.Adet}");
+//            }
+//        }
+//    }
+
+//    static void Main(string[] args)
+//    {
+//        Depo depo = new Depo();
+//        decimal bakiye = 1000m;  
+
+//        while (true)
+//        {
+//            Console.WriteLine("\n1. Ürün Ekle");
+//            Console.WriteLine("2. Ürün Sat");
+//            Console.WriteLine("3. Depoyu Listele");
+//            Console.WriteLine("4. Çıkış");
+//            Console.Write("Seçiminizi yapın: ");
+//            string secim = Console.ReadLine();
+
+//            if (secim == "1")
+//            {
+//                Console.Write("Ürün adı: ");
+//                string adi = Console.ReadLine();
+
+//                Console.Write("Birim fiyatı: ");
+//                decimal birimFiyat = Convert.ToDecimal(Console.ReadLine());
+
+//                Console.Write("Adet: ");
+//                int adet = Convert.ToInt32(Console.ReadLine());
+
+//                depo.UrunEkle(adi, birimFiyat, adet);
+//            }
+//            else if (secim == "2")
+//            {
+//                Console.Write("Satmak istediğiniz ürün adı: ");
+//                string adi = Console.ReadLine();
+
+//                Console.Write("Satmak istediğiniz adet: ");
+//                int adet = Convert.ToInt32(Console.ReadLine());
+
+//                Console.WriteLine($"Mevcut bakiyeniz: {bakiye} TL");
+
+//                if (depo.UrunSat(adi, adet, bakiye))
+//                {
+//                    decimal toplamTutar = depo.Urunler.Find(u => u.Adi == adi).BirimFiyat * adet;
+//                    bakiye -= toplamTutar;  // Satış yapıldıktan sonra bakiyeyi düşür
+//                }
+//            }
+//            else if (secim == "3")
+//            {
+//                depo.UrunleriListele();
+//            }
+//            else if (secim == "4")
+//            {
+//                break;
+//            }
+//            else
+//            {
+//                Console.WriteLine("Geçersiz seçim, tekrar deneyin.");
+//            }
+//        }
+//    }
+//}
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
-    
-    public class Urun
-    {
-        public string Adi { get; set; }
-        public decimal BirimFiyat { get; set; }
-        public int Adet { get; set; }
-
-        public Urun(string adi, decimal birimFiyat, int adet)
-        {
-            Adi = adi;
-            BirimFiyat = birimFiyat;
-            Adet = adet;
-        }
-    }
-
-    
-    public class Depo
-    {
-        public List<Urun> Urunler { get; set; }
-
-        public Depo()
-        {
-            Urunler = new List<Urun>();
-        }
-
-        public void UrunEkle(string adi, decimal birimFiyat, int adet)
-        {
-            var mevcutUrun = Urunler.Find(u => u.Adi == adi);
-            if (mevcutUrun != null)
-            {
-                mevcutUrun.Adet += adet;
-            }
-            else
-            {
-                Urunler.Add(new Urun(adi, birimFiyat, adet));
-            }
-            Console.WriteLine($"{adi} ürünü depoya eklendi.");
-        }
-
-       
-        public bool UrunSat(string adi, int adet, decimal param)
-        {
-            var urun = Urunler.Find(u => u.Adi == adi);
-            if (urun == null)
-            {
-                Console.WriteLine("Depoda bu ürün bulunmamaktadır.");
-                return false;
-            }
-            if (urun.Adet < adet)
-            {
-                Console.WriteLine("Depoda yeterli adet yok.");
-                return false;
-            }
-
-            decimal toplamTutar = urun.BirimFiyat * adet;
-            if (param < toplamTutar)
-            {
-                Console.WriteLine("Yeterli paranız yok.");
-                return false;
-            }
-
-            urun.Adet -= adet;
-            Console.WriteLine($"{adet} adet {adi} satıldı. Toplam tutar: {toplamTutar} TL.");
-            return true;
-        }
-
-        
-        public void UrunleriListele()
-        {
-            if (Urunler.Count == 0)
-            {
-                Console.WriteLine("Depoda hiç ürün yok.");
-                return;
-            }
-            foreach (var urun in Urunler)
-            {
-                Console.WriteLine($"Ürün: {urun.Adi}, Birim Fiyatı: {urun.BirimFiyat} TL, Stok: {urun.Adet}");
-            }
-        }
-    }
+    static List<Book> books = new List<Book>();
+    static List<Rental> rentals = new List<Rental>();
 
     static void Main(string[] args)
     {
-        Depo depo = new Depo();
-        decimal bakiye = 1000m;  
-
         while (true)
         {
-            Console.WriteLine("\n1. Ürün Ekle");
-            Console.WriteLine("2. Ürün Sat");
-            Console.WriteLine("3. Depoyu Listele");
-            Console.WriteLine("4. Çıkış");
+            ShowHeader("KÜTÜPHANE PROGRAMI");
+            Console.WriteLine("1. Kitap Ekle");
+            Console.WriteLine("2. Kitap Kirala");
+            Console.WriteLine("3. Kitap İade");
+            Console.WriteLine("4. Kitap Arama");
+            Console.WriteLine("5. Raporlama");
+            Console.WriteLine("H. Yardım");
+            Console.WriteLine("0. Çıkış");
             Console.Write("Seçiminizi yapın: ");
-            string secim = Console.ReadLine();
 
-            if (secim == "1")
+            string choice = Console.ReadLine();
+            switch (choice.ToUpper())
             {
-                Console.Write("Ürün adı: ");
-                string adi = Console.ReadLine();
-
-                Console.Write("Birim fiyatı: ");
-                decimal birimFiyat = Convert.ToDecimal(Console.ReadLine());
-
-                Console.Write("Adet: ");
-                int adet = Convert.ToInt32(Console.ReadLine());
-
-                depo.UrunEkle(adi, birimFiyat, adet);
+                case "1":
+                    AddBook();
+                    break;
+                case "2":
+                    RentBook();
+                    break;
+                case "3":
+                    ReturnBook();
+                    break;
+                case "4":
+                    SearchBook();
+                    break;
+                case "5":
+                    GenerateReport();
+                    break;
+                case "H":
+                    ShowHelp();
+                    break;
+                case "0":
+                    ShowMessage("Çıkış yapılıyor...", ConsoleColor.Yellow);
+                    return;
+                default:
+                    ShowMessage("Geçersiz seçim. Lütfen 1-5 arası bir seçenek girin.", ConsoleColor.Red);
+                    break;
             }
-            else if (secim == "2")
-            {
-                Console.Write("Satmak istediğiniz ürün adı: ");
-                string adi = Console.ReadLine();
 
-                Console.Write("Satmak istediğiniz adet: ");
-                int adet = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine($"Mevcut bakiyeniz: {bakiye} TL");
-
-                if (depo.UrunSat(adi, adet, bakiye))
-                {
-                    decimal toplamTutar = depo.Urunler.Find(u => u.Adi == adi).BirimFiyat * adet;
-                    bakiye -= toplamTutar;  // Satış yapıldıktan sonra bakiyeyi düşür
-                }
-            }
-            else if (secim == "3")
-            {
-                depo.UrunleriListele();
-            }
-            else if (secim == "4")
-            {
-                break;
-            }
-            else
-            {
-                Console.WriteLine("Geçersiz seçim, tekrar deneyin.");
-            }
+            Console.WriteLine("\nDevam etmek için bir tuşa basın...");
+            Console.ReadKey();
         }
     }
+
+    static void ShowHelp()
+    {
+        ShowHeader("YARDIM MENÜSÜ");
+        Console.WriteLine("1. Kitap Ekleme: Kitap adı, yazar adı, yayınevi ve stok bilgisi girilerek kitap eklenir.");
+        Console.WriteLine("2. Kitap Kiralama: Mevcut kitaplardan birini seçip kaç gün kiralayacağınız belirlenir.");
+        Console.WriteLine("3. Kitap İade: Kiralanan kitapları iade edebilirsiniz.");
+        Console.WriteLine("4. Arama: Kitap adı ya da yazar adı ile kitap arayabilirsiniz.");
+        Console.WriteLine("5. Raporlama: Kitaplar ve kiralama raporları görüntülenir.");
+        Console.WriteLine("6. Günlük Kiralama Bedeli: 5 TL");
+        Console.WriteLine("7. Maksimum Kiralama Süresi: 14 gündür.");
+        Console.WriteLine("8. Kitap iadesi yapılmazsa, belirli bir ceza uygulanacaktır.");
+    }
+
+    static void AddBook()
+    {
+        ShowHeader("KITAP EKLE");
+        Console.Write("Kitap Adı: ");
+        string title = Console.ReadLine();
+
+        Console.Write("Yazar Adı: ");
+        string author = Console.ReadLine();
+
+        Console.Write("Yayın Yılı: ");
+        int year = int.Parse(Console.ReadLine());
+
+        Console.Write("Stok Miktarı: ");
+        int stock = int.Parse(Console.ReadLine());
+
+        var existingBook = books.FirstOrDefault(b => b.Title == title && b.Author == author);
+        if (existingBook != null)
+        {
+            existingBook.Stock += stock;
+            ShowMessage("Mevcut kitap bulundu. Stok artırıldı.", ConsoleColor.Green);
+        }
+        else
+        {
+            books.Add(new Book { Title = title, Author = author, Year = year, Stock = stock });
+            ShowMessage("Yeni kitap başarıyla eklendi.", ConsoleColor.Green);
+        }
+    }
+
+    static void RentBook()
+    {
+        ShowHeader("KITAP KIRALA");
+        if (!books.Any())
+        {
+            ShowMessage("Kütüphanede kitap bulunmamaktadır.", ConsoleColor.Red);
+            return;
+        }
+
+        Console.WriteLine("Mevcut Kitaplar:");
+        for (int i = 0; i < books.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {books[i].Title} - {books[i].Author} (Stok: {books[i].Stock})");
+        }
+
+        Console.Write("Kiralamak istediğiniz kitabın numarasını seçin: ");
+        int bookIndex = int.Parse(Console.ReadLine()) - 1;
+
+        if (bookIndex < 0 || bookIndex >= books.Count || books[bookIndex].Stock <= 0)
+        {
+            ShowMessage("Geçersiz seçim veya stok yetersiz.", ConsoleColor.Red);
+            return;
+        }
+
+        Console.Write("Kaç gün kiralamak istiyorsunuz? (Maksimum 14 gün): ");
+        int days = int.Parse(Console.ReadLine());
+
+        if (days > 14)
+        {
+            ShowMessage("Kiralama süresi maksimum 14 gün olmalıdır.", ConsoleColor.Red);
+            return;
+        }
+
+        decimal cost = days * 5;
+        Console.Write($"Bütçeniz nedir? (Gerekli ücret: {cost} TL): ");
+        decimal budget = decimal.Parse(Console.ReadLine());
+
+        if (budget < cost)
+        {
+            ShowMessage("Bütçeniz yetersiz. Kiralama işlemi yapılamadı.", ConsoleColor.Red);
+            return;
+        }
+
+        Console.Write("Adınız: ");
+        string userName = Console.ReadLine();
+
+        books[bookIndex].Stock--;
+        rentals.Add(new Rental
+        {
+            UserName = userName,
+            RentedBook = books[bookIndex],
+            RentDate = DateTime.Now,
+            ReturnDate = DateTime.Now.AddDays(days)
+        });
+
+        ShowMessage($"Kitap başarıyla kiralandı. Ödediğiniz tutar: {cost} TL. İade tarihi: {DateTime.Now.AddDays(days):yyyy-MM-dd}.", ConsoleColor.Green);
+    }
+
+    static void ReturnBook()
+    {
+        ShowHeader("KITAP IADE");
+        Console.Write("Adınız: ");
+        string userName = Console.ReadLine();
+
+        var userRentals = rentals.Where(r => r.UserName == userName).ToList();
+        if (!userRentals.Any())
+        {
+            ShowMessage("Bu isimle kiralanmış bir kitap bulunamadı.", ConsoleColor.Red);
+            return;
+        }
+
+        Console.WriteLine("Kiralanan Kitaplar:");
+        for (int i = 0; i < userRentals.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {userRentals[i].RentedBook.Title} (İade Tarihi: {userRentals[i].ReturnDate:yyyy-MM-dd})");
+        }
+
+        Console.Write("İade etmek istediğiniz kitabın numarasını seçin: ");
+        int rentalIndex = int.Parse(Console.ReadLine()) - 1;
+
+        if (rentalIndex < 0 || rentalIndex >= userRentals.Count)
+        {
+            ShowMessage("Geçersiz seçim. Lütfen tekrar deneyin.", ConsoleColor.Red);
+            return;
+        }
+
+        var rental = userRentals[rentalIndex];
+        rental.RentedBook.Stock++;
+        rentals.Remove(rental);
+        ShowMessage("Kitap başarıyla iade edildi.", ConsoleColor.Green);
+    }
+
+    static void SearchBook()
+    {
+        ShowHeader("KITAP ARAMA");
+        Console.WriteLine("1. Kitap Adına Göre");
+        Console.WriteLine("2. Yazar Adına Göre");
+        Console.Write("Seçiminiz: ");
+        if (!int.TryParse(Console.ReadLine(), out int searchType) || (searchType != 1 && searchType != 2))
+        {
+            ShowMessage("Geçersiz seçim. Lütfen 1 veya 2 girin.", ConsoleColor.Red);
+            return;
+        }
+
+        Console.Write("Arama terimini girin: ");
+        string searchTerm = Console.ReadLine();
+
+        var results = searchType == 1
+            ? books.Where(b => b.Title.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)).ToList()
+            : books.Where(b => b.Author.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)).ToList();
+
+        if (!results.Any())
+        {
+            ShowMessage("Hiçbir sonuç bulunamadı.", ConsoleColor.Red);
+            return;
+        }
+
+        Console.WriteLine("Arama Sonuçları:");
+        foreach (var book in results)
+        {
+            Console.WriteLine($"{book.Title} - {book.Author} ({book.Year}) | Stok: {book.Stock}");
+        }
+    }
+
+    static void GenerateReport()
+    {
+        ShowHeader("RAPORLAMA");
+        Console.WriteLine("1. Tüm Kitaplar");
+        Console.WriteLine("2. Kirada Olan Kitaplar");
+        Console.Write("Seçiminizi yapın: ");
+        string choice = Console.ReadLine();
+
+        if (choice == "1")
+        {
+            Console.WriteLine("Tüm Kitaplar:");
+            foreach (var book in books)
+            {
+                Console.WriteLine($"{book.Title} - {book.Author} ({book.Year}) | Stok: {book.Stock}");
+            }
+        }
+        else if (choice == "2")
+        {
+            Console.WriteLine("Kirada Olan Kitaplar:");
+            foreach (var rental in rentals)
+            {
+                Console.WriteLine($"{rental.RentedBook.Title} - {rental.UserName} (İade Tarihi: {rental.ReturnDate:yyyy-MM-dd})");
+            }
+        }
+        else
+        {
+            ShowMessage("Geçersiz seçim. Lütfen 1 veya 2 girin.", ConsoleColor.Red);
+        }
+    }
+
+    static void ShowHeader(string title)
+    {
+        Console.Clear();
+        Console.WriteLine(title);
+        Console.WriteLine(new string('=', title.Length));
+    }
+
+    static void ShowMessage(string message, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+}
+
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+    public int Stock { get; set; }
+}
+
+class Rental
+{
+    public string UserName { get; set; }
+    public Book RentedBook { get; set; }
+    public DateTime RentDate { get; set; }
+    public DateTime ReturnDate { get; set; }
 }
